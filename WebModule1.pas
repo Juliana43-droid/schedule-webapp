4f483c8482e9d16e79f9b96b5076c1bad32b9b0c
Juliana43-droid <yulianamackarevitch@yandex.by>
2025-07-10 16:18:40 +0300

    Create WebModule1.pas

---
+++ WebModule1.pas
@@ -0,0 +1,27 @@
+unit WebModule1;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, Web.HTTPApp;
+
+type
+  TWebModule1 = class(TWebModule)
+    procedure WebModule1DefaultHandlerAction(Sender: TObject;
+      Request: TWebRequest; Response: TWebResponse; var Handled: Boolean);
+  end;
+
+var
+  WebModule1: TWebModule1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TWebModule1.WebModule1DefaultHandlerAction(Sender: TObject;
+  Request: TWebRequest; Response: TWebResponse; var Handled: Boolean);
+begin
+  Response.Content := '<html><body><h2>Добро пожаловать в систему "Расписание занятий"!</h2></body></html>';
+end;
+
+end.
